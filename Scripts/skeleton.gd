aac92c77a7055f1965c33fde16e20223f5eff8f3
victoriaaa111 <victoria.mutruc@gmail.com>
2025-05-18 21:15:35 +0300

    Make enemy change walking animation based on direction

--- Scripts/skeleton.gd
+++ Scripts/skeleton.gd
@@ -7,7 +7,20 @@ var player = null
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
+		if (player.position.y - position.y) < -10:
+			$AnimatedSprite2D.play("back_walking")	
+		elif(player.position.y - position.y) < 30:
+			$AnimatedSprite2D.play("walk")
+			
+			if(player.position.x - position.x) < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
+		elif (player.position.y - position.y) > 30:
+			$AnimatedSprite2D.play("front_walking")
 		
+	else:
+		$AnimatedSprite2D.play("idle")
 	
 func _on_detection_area_body_entered(body):
 	player = body
